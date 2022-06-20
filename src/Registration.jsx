a38647d12288264f6425cd5e1f0a6e75745ad081
Anusha.ca <Anushaca1996@gmail.com>
2022-06-20 12:13:46 +0530

    Confirmation tab

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -2,42 +2,47 @@ import React from "react";
 import "./Registration.css"
 
 function Registration(props) {
-    
+
     return (
         <div>
             {/* <p>{props.name}</p>
            <p>{props.id}</p> */}
-            
+
             <form className="body">
                 <div class="container">
                     <h5>Get Registered</h5>
                     <div>
-                     <div className="radio">
-                     <input type="radio"/> <label >I am Elena Garcia-Davranova</label><br></br>
-                    </div>   
-                    
-                    <div className="radio1">
-                    <input type="radio"/> <label >I am an Employeee of the Elena Garcia-Davranova's practice or hospital</label><br></br>
-                    </div>
-                    
-                    <div className="radio1">
-                    <input type="radio"/> <label >I am a third party firm representing the Elena Garcia-Davranova (not the practice manager or admin)</label><br></br>
-                    </div>
-                    
+                        <div className="radio">
+                            <input type="radio" /> <label >I am Elena Garcia-Davranova</label><br></br>
+                        </div>
+
+                        <div className="radio1">
+                            <input type="radio" /> <label >I am an Employeee of the Elena Garcia-Davranova's practice or hospital</label><br></br>
+                        </div>
+
+                        <div className="radio1">
+                            <input type="radio" /> <label >I am a third party firm representing the Elena Garcia-Davranova (not the practice manager or admin)</label><br></br>
+                        </div>
+
                     </div>
 
                     <label for="email" className="lable1"><b>Email</b></label><br></br>
-                    <input type="text" placeholder="Enter your Email" name="email" id="email" required/><br></br>
+                    <input type="text" placeholder="Enter your Email" name="email" id="email" required /><br></br>
 
                     <label for="psw" className="lable"><b>Password</b></label><br></br>
-                    <input type="password" placeholder="Create New Password" name="psw" id="psw" required/><br></br>
+                    <input type="password" placeholder="Create New Password" name="psw" id="psw" required /><br></br>
 
                     <div className="checkbox">
-                    <input type="checkbox" /> <label>I agree to the <a><b>Healthgrades User Agreement</b></a> </label><br></br>
+                        <input type="checkbox" /> <label>I agree to the <a><b>Healthgrades User Agreement</b></a> </label><br></br>
                     </div>
-                    <button type="button">Cancel</button>
                     
-                    <button type="button">Continue</button>
+                    <button type="button" className="button1">Cancel</button>
+                    <div class="space">
+                    </div>
+                    <button type="button" className="button">Continue</button>
+                    
+                    
+
                 </div>
             </form>
             {/* <button onClick={CallMe}>
--- src/confirmation.jsx
+++ src/confirmation.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 
-function Confirmation(props){
-    return(
+function Confirmation(props) {
+    return (
         <div>
             <form className="body">
-            <div class="container">
-            <h5>Get confirmation</h5>
+                <div class="container">
+                    <h5 >You're almost done</h5>
+                    <p>A verification code has been sent to 720-123-1111 but will expire soon</p>
+                    <label for="verify" className="lable2"><b>Please Enter Your Verification Code</b></label><br></br>
+                    <input type="text" placeholder="Enter Your Verification Code" name="verify" id="verify" required /><br></br>
+                    <p>This code will expire in</p>
 
-            </div>
+                    <p>This code will expire immediatley if you leave this page.</p>
+
+                    <button type="button" className="button2">Submit Registration</button>
+
+                    <p>Not correct number? <a><b>Modify number</b></a></p>
+                    <hr></hr>
+                    <p><b>Didn't find what you need or still have questions?</b> That's ok, we're here to help.</p>
+
+                </div>
             </form>
         </div>
 
     );
-    
+
 }
 
 export default Confirmation;
