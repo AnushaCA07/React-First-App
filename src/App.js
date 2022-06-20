cf1770bf2fe1484ef81ce85674b307564bc70fc2
chandra <kschandra123@gmail.com>
2022-06-20 06:38:34 +0530

    Tabs added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import FirstComponent from "./FirstComponent";
 import Registration from './Registration';
 import Confirmation from './Confirmation';
 import Verify from './Verify';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 function App() {
   const [counter, increment] = React.useState(0); // state variable  
@@ -14,9 +16,28 @@ function App() {
   return (
     <div className="App">
 
-      <Registration  />
-      <Confirmation />
-      <Verify/>
+
+<Tabs>
+
+    <TabList>
+      <Tab> Registration </Tab>
+      <Tab> Confirmation </Tab>
+      <Tab> Verify </Tab>
+    </TabList>
+
+    <TabPanel>
+    <Registration  />
+    </TabPanel>
+    <TabPanel>
+    <Confirmation />
+    </TabPanel>
+    <TabPanel>
+    <Verify/>
+    </TabPanel>
+  </Tabs>
+
+      
+      
     </div>
   );
 }
